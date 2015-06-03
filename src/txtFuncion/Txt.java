a14417704c40a6d459d684f3b802481cc859af72
Alexandre Gonçalves Ferreira <alex.taru@yahoo.com.br>
2015-06-03 17:26:39 -0300

    criando função para passar array de string para inteiro e organizar em ordem crescente

--- src/txtFuncion/Txt.java
+++ src/txtFuncion/Txt.java
@@ -8,18 +8,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class Txt {
-	private String resultados,linha,numj;
-	int tamanho;
-	private String[] numeros = new String[5];
+	private String resultados;
+	private int tamanho;
+	
+	
 
 	public Txt() {
 		resultados = null;
-		tamanho = 0;
-		numj = null;
-		
+		tamanho = 1;//numeros de linhas do arquivo
 	}
 	
 	//gerar um txt com os jogos do arquivo html	
@@ -43,38 +44,66 @@ public class Txt {
 	
 	//acha resultados igual
 	public void Igualdade(){
-			BufferedReader file = null;
-			int inicio,fim;
+		String temporario[] = new String[5];
+		int numeros[] = new int[5];
+		int numeros2[] = new int[5];
+		String linha,numj,data;
+		int nlinha = 0,temp;//linha que esta comparando
+		
+		BufferedReader file = null;
+		int inicio,fim;
 			
-			try {
-				file = new BufferedReader(new FileReader("arquivos/resutados.txt"));
-			} catch (FileNotFoundException e) {
-				System.err.println("Erro ao ler arquivo resutados.txt");
-				e.printStackTrace();
-			}
-			
-			try {
-				for (int cont = 0;cont<1;cont++){
-					while (file.ready()){
+		try {
+			file = new BufferedReader(new FileReader("arquivos/resutados.txt"));
+		} catch (FileNotFoundException e) {
+			System.err.println("Erro ao ler arquivo resutados.txt");
+			e.printStackTrace();
+		}
+	
+		try {
+			for (int cont = 0;cont<tamanho;cont++){//comparar linha por linha
+				while (file.ready()){
+					if(cont == 0){
 						linha = file.readLine();
+						tamanho = Integer.parseInt(linha);
+						cont++;
+						nlinha++;
+					}else{
+						
+						if(cont == nlinha){
+							
+							linha = file.readLine();
 					
-						inicio = linha.indexOf("<");
-						fim = linha.indexOf(">");
+							inicio = linha.indexOf("<");
+							fim = linha.indexOf(">");
 						
-						numj = linha.substring(inicio + 1, fim);
-						
-						
-						
+							numj = linha.substring(inicio + 1, fim);
+							data = linha.substring(0,inicio);
+							
+							temporario = numj.split (Pattern.quote (";"));
+							
+							for(int lin = 0;lin < 5;lin++){
+								numeros[lin] = Integer.parseInt(temporario[lin]);
+							}
+							
+							for(int cont1 = 0;cont1<5;cont1++){
+								for(int cont2 = cont1+1;cont2<5;cont2++){
+									if(numeros[cont1] > numeros[cont2]){
+										temp = numeros[cont1];
+										numeros[cont1] = numeros[cont2];
+										numeros[cont2] = temp;
+									}
+								}
+							}
+							
+							
+						}
 					}
 				}
-			} catch (IOException e) {
-				System.err.println("Erro ao gravar arquivo resutados.txt");
-				e.printStackTrace();
 			}
-			
-			
+		} catch (IOException e) {
+			System.err.println("Erro ao gravar arquivo resutados.txt");
+			e.printStackTrace();
+		}		
 	}
-	
-	
-
 }
