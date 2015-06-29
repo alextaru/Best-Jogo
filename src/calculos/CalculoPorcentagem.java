a24f799bf34503f5211c6faa06e9f48103a776e4
Alexandre Gonçalves Ferreira <alex.taru@yahoo.com.br>
2015-06-29 16:44:21 -0300

    cirando classe para calcular porcentagem, classe ainda não terminada

---
+++ src/calculos/CalculoPorcentagem.java
@@ -0,0 +1,14 @@
+package calculos;
+
+public class CalculoPorcentagem {
+
+	public CalculoPorcentagem() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//calcula a porcentagem em que um numero sai em pereiodo
+	public void CalculoPorcentagemNumero(String local){
+		
+	}
+
+}
