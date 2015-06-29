bfe4e3b4c2f72d7e0dab84b9590ffb15e7ee562f
Alexandre Gonçalves Ferreira <alex.taru@yahoo.com.br>
2015-06-29 16:42:46 -0300

    removendo função de organizava os arquivo pro mês e colocando em uma classe separada

--- src/txtFuncion/Txt.java
+++ src/txtFuncion/Txt.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
-import organizador.OrganizaVariavel;
+import organizador.Organiza;
 
 
 public class Txt {
@@ -19,7 +19,7 @@ public class Txt {
 	int inicio,fim;
 	String data;
 	int tamanho;
-	OrganizaVariavel organiza = new OrganizaVariavel();
+	Organiza organiza = new Organiza();
 	
 	
 	
@@ -34,8 +34,9 @@ public class Txt {
 		tamanho = 0;
 	}
 	
-	//adiciona as linhas do arquivo txt ao arrayList
-	public void GravarTxt(String arquivo){
+	//retorna as linhas do arquivo txt em arrayList
+	public ArrayList<String> GravarTxt(String arquivo){
+		ArrayList<String> linhasArquivo = new ArrayList<String>();
 		
 		try {
 			file = new BufferedReader(new FileReader(arquivo));
@@ -46,14 +47,14 @@ public class Txt {
 			
 		try {
 			while (file.ready()){//adiciona as linhas do arquivo em uma array
-				linhas.add(file.readLine());
+				linhasArquivo.add(file.readLine());
 			}
 		}catch (IOException e) {
 			System.err.println("Erro ao gravar arquivo resutados.txt no array");
 			e.printStackTrace();
 		}
 		
-		tamanho = linhas.size();
+		return linhasArquivo;
 	}
 	
 	//gerar um txt com os jogos do arquivo html	
@@ -69,118 +70,7 @@ public class Txt {
 		}
 	}
 	
-	//separa todos os resultados por mes
-	public void MesSeparetor(){
-		String mes1 = null,mes2 = null,mes3 = null,mes4 = null,mes5 = null,mes6 = null,mes7 = null,mes8 = null,mes9 = null,mes10 = null,mes11 = null,mes12 = null;
-		String temp;
-		int primeira,ultima,mes;
-		
-		GravarTxt("arquivos/resutados.txt");
-		
-		for (int cont = 0;cont<tamanho;cont++){
-			inicio = linhas.get(cont).indexOf("<");
-			fim = linhas.get(cont).indexOf(">");
-			data = linhas.get(cont).substring(0,inicio);
-			
-			primeira = data.indexOf("/");
-			ultima = data.lastIndexOf("/");
-			
-			temp = data.substring(primeira+1,ultima);
-			
-			mes = Integer.parseInt(temp);
-			
-			switch(mes){//adicionar cada resultado em seu mes
-				case 1:
-					if(mes1 == null) 
-						mes1 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else	
-						mes1 = mes1 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 2:
-					if(mes2 == null)
-						mes2 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes2 = mes2 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 3:
-					if(mes3 == null)
-						mes3 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes3 = mes3 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 4:
-					if(mes4 == null)
-						mes4 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes4 = mes4 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 5:
-					if(mes5 == null)
-						mes5 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes5 = mes5 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 6:
-					if(mes6 == null)
-						mes6 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes6 = mes6 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 7:
-					if(mes7 == null)
-						mes7 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes7 = mes7 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 8:
-					if(mes8 == null)
-						mes8 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes8 = mes8 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 9:
-					if(mes9 == null)
-						mes9 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes9 = mes9 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 10:
-					if(mes10 == null)
-						mes10 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes10 = mes10 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 11:
-					if(mes11 == null)
-						mes11 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes11 = mes11 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-				case 12:
-					if(mes12 == null)
-						mes12 = linhas.get(cont).substring(inicio+1,fim) + "\n";
-					else
-						mes12 = mes12 + linhas.get(cont).substring(inicio+1,fim) + "\n";
-					break;
-
-			}
-			
-		}
-		
-		//criando os arquivos com o conteudo do mes
-		TxtGerador("arquivos/mes1.txt",mes1);
-		TxtGerador("arquivos/mes2.txt",mes2);
-		TxtGerador("arquivos/mes3.txt",mes3);
-		TxtGerador("arquivos/mes4.txt",mes4);
-		TxtGerador("arquivos/mes5.txt",mes5);
-		TxtGerador("arquivos/mes6.txt",mes6);
-		TxtGerador("arquivos/mes7.txt",mes7);
-		TxtGerador("arquivos/mes8.txt",mes8);
-		TxtGerador("arquivos/mes9.txt",mes9);
-		TxtGerador("arquivos/mes10.txt",mes10);
-		TxtGerador("arquivos/mes11.txt",mes12);
-		TxtGerador("arquivos/mes12.txt",mes12);
-	}
+	
 	
 	
 	//acha resultados igual
