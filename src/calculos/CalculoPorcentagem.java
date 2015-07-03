4defc6cd133fe9ca1d30677faa7a5d6c1e53ee10
Alexandre Gonçalves Ferreira <alex.taru@yahoo.com.br>
2015-07-02 22:45:43 -0300

    adicionando as funções para converter arraylist

--- src/calculos/CalculoPorcentagem.java
+++ src/calculos/CalculoPorcentagem.java
@@ -1,5 +1,10 @@
 package calculos;
 
+import java.util.ArrayList;
+
+import converter.ConverterVariavel;
+import txtFuncion.Txt;
+
 public class CalculoPorcentagem {
 
 	public CalculoPorcentagem() {
@@ -8,6 +13,14 @@ public class CalculoPorcentagem {
 	
 	//calcula a porcentagem em que um numero sai em pereiodo
 	public void CalculoPorcentagemNumero(String local){
+		ArrayList<Integer> numerosJogos = new ArrayList<Integer>();
+		int tamanhoArray;
+		
+		Txt txt = new Txt();
+		ConverterVariavel converte = new ConverterVariavel();
+		
+		numerosJogos = converte.CoverteParaInteiro(txt.GravarTxt(local));
+		
 		
 	}
 
