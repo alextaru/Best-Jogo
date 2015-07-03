ea97cd44c70d670bffdf6c270aba26dbaffba92e
Alexandre Gonçalves Ferreira <alex.taru@yahoo.com.br>
2015-07-02 22:41:33 -0300

    Criando classe que converte variáveis, foi criada a que converte string em inteiro

---
+++ src/converter/ConverterVariavel.java
@@ -0,0 +1,25 @@
+package converter;
+
+import java.util.ArrayList;
+
+public class ConverterVariavel {
+
+	public ConverterVariavel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//funcao para converter String para inteiro
+	public ArrayList<Integer> CoverteParaInteiro(ArrayList<String> numerosCoverter){
+		int tamanhoArray;
+		ArrayList<Integer> numerosConvertidos = new ArrayList<Integer>();
+		
+		tamanhoArray = numerosCoverter.size();
+		
+		for(int cont=0; cont < tamanhoArray; cont++){
+			numerosConvertidos.add(Integer.parseInt(numerosCoverter.get(cont)));
+		}
+		
+		return numerosConvertidos;
+	}
+
+}
