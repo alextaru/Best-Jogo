6db61fbd0ca7fe011d5a145ca1216108ed5c38db
Alexandre Gonçalves Ferreira <alex.taru@yahoo.com.br>
2015-07-10 12:34:44 -0300

    Criando as telas principal, porcentagem e igualdades

--- src/Grafico/Principal.java
+++ src/Grafico/Principal.java
@@ -1,57 +1,29 @@
 package Grafico;
 
-import imporHtml.HtmlFunc;
-
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JLayeredPane;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSeparator;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import javax.swing.SwingConstants;
-import javax.swing.JRadioButton;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
-
-import calculos.CalculoPorcentagem;
-import organizador.Organiza;
-import txtFuncion.Txt;
 
 public class Principal {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		//Organiza organizar = new Organiza();
-		//HtmlFunc html = new HtmlFunc();
-		//Txt txt = new Txt();
-		CalculoPorcentagem cauculo = new CalculoPorcentagem();
 		
-		cauculo.CalculoPorcentagemNumero("arquivos/mes4.txt");
-		
-		//html.HtmlRead();
-		
-		//txt.Igualdade();
-		
-		
-		//organizar.MesSeparetor();
-		
-		
-		/*EventQueue.invokeLater(new Runnable() {
+		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Principal window = new Principal();
@@ -60,95 +32,151 @@ public class Principal {
 					e.printStackTrace();
 				}
 			}
-		});*/
+		});
 		
 		
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public Principal() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(0, 0, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.setLocationRelativeTo(null);
 		
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 800, 28);
+		menuBar.setBounds(0, 0, 800, 22);
 		frame.getContentPane().add(menuBar);
+		frame.setLocationRelativeTo(null);
 		
-		JMenu analizar = new JMenu("Analizar Jogo");
-		menuBar.add(analizar);
 		
-		JMenuItem mntmResultadosIgual = new JMenuItem("resultados Igual");
+		JMenu menu = new JMenu("Menu");
+		menuBar.add(menu);
 		
-		analizar.add(mntmResultadosIgual);
+		JMenuItem mntmResultadosIguais = new JMenuItem("Resultados iguais");
+		menu.add(mntmResultadosIguais);
+		
+		JMenuItem mntmPorcentagemPorPeriodo = new JMenuItem("Porcentagem por periodo");
+		menu.add(mntmPorcentagemPorPeriodo);
 		
 		JSeparator separator = new JSeparator();
-		separator.setOrientation(SwingConstants.VERTICAL);
-		menuBar.add(separator);
+		menu.add(separator);
 		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		menu.add(mntmSair);
 		
-		final JLayeredPane IgualdadePane = new JLayeredPane();
-		IgualdadePane.setBounds(0, 28, 800,550);
-		frame.getContentPane().add(IgualdadePane);
+		final JPanel panelIguais = new JPanel();
+		panelIguais.setBounds(0, 22, 800, 556);
+		frame.getContentPane().add(panelIguais);
+		panelIguais.setLayout(null);
+		panelIguais.setVisible(false);
 		
-		final JRadioButton rdbtnTodosOsResultados = new JRadioButton("todos os resultados");
-		rdbtnTodosOsResultados.setBounds(35, 62, 157, 23);
-		IgualdadePane.add(rdbtnTodosOsResultados);
+		final JPanel panelPorcentagem = new JPanel();
+		panelPorcentagem.setBounds(0, 22, 800, 556);
+		frame.getContentPane().add(panelPorcentagem);
+		panelPorcentagem.setLayout(null);
+		panelPorcentagem.setVisible(false);
 		
-		JLabel lblMarqueAbaixoO = new JLabel("Marque abaixo o periodo que voce deseja fazer a comparacao.");
-		lblMarqueAbaixoO.setBounds(177, 17, 435, 16);
-		IgualdadePane.add(lblMarqueAbaixoO);
+		final JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setBounds(0, 22, 800, 556);
+		frame.getContentPane().add(panelPrincipal);
+		panelPrincipal.setLayout(null);
 		
-		final JTextArea textAreaResult = new JTextArea();
-		textAreaResult.setBounds(0, 112, 800, 438);
-		IgualdadePane.add(textAreaResult);
-		IgualdadePane.setVisible(false);
+		JLabel lblSelecioneOPeriodo = new JLabel("Selecione o periodo para mostrar a porcentagem de cada numero por sorteio");
+		lblSelecioneOPeriodo.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblSelecioneOPeriodo.setBounds(113, 49, 573, 21);
+		panelPorcentagem.add(lblSelecioneOPeriodo);
 		
-		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.addActionListener(new ActionListener() {
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Todos resultados", "Janeiro", "Fevereiro", "Mar\u00E7o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"}));
+		comboBox.setBounds(205, 95, 165, 27);
+		panelPorcentagem.add(comboBox);
+		
+		JButton btnAnalizar = new JButton("Analizar");
+		btnAnalizar.setBounds(382, 94, 117, 29);
+		panelPorcentagem.add(btnAnalizar);
+		
+		JList listPorcentagem = new JList();
+		listPorcentagem.setBounds(6, 155, 788, 327);
+		panelPorcentagem.add(listPorcentagem);
+		
+		JButton btnVoltarPorcentagem = new JButton("Voltar");
+		btnVoltarPorcentagem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (rdbtnTodosOsResultados.isSelected()){
-					BufferedReader file = null;
-					
-					try {
-						file = new BufferedReader(new FileReader("arquivos/resulIgual.txt"));
-					} catch (FileNotFoundException e1) {
-						System.err.println("Erro ao ler arquivo resulIgual.txt");
-						e1.printStackTrace();
-					}
-					
-					try {
-						while (file.ready()){//adiciona as linhas do arquivo em uma array
-							textAreaResult.append(file.readLine());
-							textAreaResult.append("\n");
-						}
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					
-				}
+				panelPorcentagem.setVisible(false);
+				panelPrincipal.setVisible(true);
 			}
 		});
-		btnBuscar.setBounds(215, 61, 117, 29);
-		IgualdadePane.add(btnBuscar);
+		btnVoltarPorcentagem.setBounds(6, 6, 93, 29);
+		panelPorcentagem.add(btnVoltarPorcentagem);
 		
-		mntmResultadosIgual.addActionListener(new ActionListener() {
+		JLabel lblSena = new JLabel("Sena");
+		lblSena.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblSena.setBounds(100, 76, 61, 16);
+		panelIguais.add(lblSena);
+		
+		JList listSena = new JList();
+		listSena.setBounds(22, 104, 210, 400);
+		panelIguais.add(listSena);
+		
+		JList listQuina = new JList();
+		listQuina.setBounds(263, 104, 210, 400);
+		panelIguais.add(listQuina);
+		
+		JList listQuadra = new JList();
+		listQuadra.setBounds(519, 104, 210, 400);
+		panelIguais.add(listQuadra);
+		
+		JLabel lblQuina = new JLabel("Quina");
+		lblQuina.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblQuina.setBounds(342, 76, 61, 16);
+		panelIguais.add(lblQuina);
+		
+		JLabel lblQuadra = new JLabel("Quadra");
+		lblQuadra.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblQuadra.setBounds(601, 76, 61, 16);
+		panelIguais.add(lblQuadra);
+		
+		JLabel lblResultadosSemelhantesComparando = new JLabel("Resultados semelhantes comparando todos os jogos j\u00E1 realizados ");
+		lblResultadosSemelhantesComparando.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		lblResultadosSemelhantesComparando.setBounds(92, 30, 594, 22);
+		panelIguais.add(lblResultadosSemelhantesComparando);
+		
+		JButton btnVoltarIguais = new JButton("Voltar");
+		btnVoltarIguais.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				IgualdadePane.setVisible(true);
+				panelIguais.setVisible(false);
+				panelPrincipal.setVisible(true);
 			}
 		});
+		btnVoltarIguais.setBounds(6, 6, 81, 29);
+		panelIguais.add(btnVoltarIguais);
+		
+		
+		
+		JButton btnResultadosIguais = new JButton("Resultados iguais");
+		btnResultadosIguais.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				panelIguais.setVisible(true);
+				panelPrincipal.setVisible(false);
+			}
+		});
+		btnResultadosIguais.setBounds(124, 106, 228, 53);
+		panelPrincipal.add(btnResultadosIguais);
+		
+		JButton btnPorcentagemDeNumeros = new JButton("Porcentagem por perido");
+		btnPorcentagemDeNumeros.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelPorcentagem.setVisible(true);
+				panelPrincipal.setVisible(false);
+			}
+		});
+		btnPorcentagemDeNumeros.setBounds(429, 106, 228, 53);
+		panelPrincipal.add(btnPorcentagemDeNumeros);
 		
 	}
 }
